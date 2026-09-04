901e4cac52343f6fd9eec7e8078a37194c4d6669
Md Ma aaz Iftekhar <mdmaaaziftekhar@gmail.com>
2026-09-04 14:31:12 +0530

    Cleaned and prepared the SMS data

--- 01_load_data.py
+++ 01_load_data.py
@@ -6,7 +6,9 @@ df = pd.read_csv("data/SMSSpamCollection", sep="	", header=None)
 # Add column names
 df.columns = ["label", "message"]
 
+# Convert labels
+df["label"] = df["label"].map({"ham": 0, "spam": 1})
+
 print(df.head())
-print(df.shape)
 print(df["label"].value_counts())
 print(df.isnull().sum())
