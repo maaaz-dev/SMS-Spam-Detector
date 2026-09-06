a66941286cefb339c9ae7770fd7420146a0454dd
Md Ma aaz Iftekhar <mdmaaaziftekhar@gmail.com>
2026-09-06 14:09:28 +0530

    Added SMS spam classification model

---
+++ 02_train_model.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+df = pd.read_csv("data/SMSSpamCollection", sep="\t", header=None)
+
+df.columns = ["label", "message"]
+
+df["label"] = df["label"].map({"ham": 0, "spam": 1})
+
+X = df["message"]
+y = df["label"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+vectorizer = TfidfVectorizer()
+
+X_train_tfidf = vectorizer.fit_transform(X_train)
+X_test_tfidf = vectorizer.transform(X_test)
+
+model = MultinomialNB()
+
+model.fit(X_train_tfidf, y_train)
+
+y_pred = model.predict(X_test_tfidf)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Model Accuracy:", accuracy)
+print(classification_report(y_test, y_pred))
+
+cm = confusion_matrix(y_test, y_pred)
+
+print("Confusion Matrix:")
+print(cm)
+
+joblib.dump(model, "spam_model.pkl")
+joblib.dump(vectorizer, "tfidf_vectorizer.pkl")
+
+print("Model and vectorizer saved successfully!")
